220a9a1c8c6cd0e8e1025ac2c15275c17f764c9b
byesteralves <byesteralves@gmail.com>
2025-10-20 22:43:38 -0300

    Main.c

---
+++ Main.c
@@ -0,0 +1,19 @@
+Main.c
+
+  #include <stdio.h>
+  #include "biblioteca.h"
+
+  Int main() {
+  printf("Bem vindo ao sistema da biblioteca escolar!\n");
+
+  // Exemplo de Uso:
+    cadastrar_livro("Anne de Green Gables", "L.M Montgomery");
+    cadastrar_usuario("Aluno01");
+
+ // Lógica de execução de sistema
+ printf("Sistema iniciado com sucesso!\n")
+
+return 0;
+}
+
+
